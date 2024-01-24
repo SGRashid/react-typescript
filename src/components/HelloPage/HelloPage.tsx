b612209b2d50bfafb7b2e9043e6efacb7062b5a3
Sergei Gabdullin <sgrashid@yandex.ru>
2024-01-24 18:16:42 +0500

    buttons were added

--- src/components/HelloPage/HelloPage.tsx
+++ src/components/HelloPage/HelloPage.tsx
@@ -1,5 +1,14 @@
-import React, { ReactNode } from "react";
+import React from "react";
+import { Link } from 'react-router-dom';
 
-const HelloPage: React.FC = () => <h1>Hello</h1>;
+const HelloPage: React.FC = () => <>
+    <h1>Hello</h1>
+    <div>
+        <Link to="/">Go Home</Link>
+    </div>
+    <div>
+        <Link to="/nothing">Go to HelloPage</Link>
+    </div>
+</>;
 
 export default HelloPage;
--- src/components/NothingPage/NothingPage.tsx
+++ src/components/NothingPage/NothingPage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function NothingPage(): JSX.Element {
-    return <h1>There is nothing here</h1>;
+    return (<>
+        <h1>There is nothing here</h1>
+        <div>
+            <Link to="/">Go Home</Link>
+        </div>
+        <div>
+            <Link to="/hello">Go to HelloPage</Link>
+        </div>
+    </>);
 }
 
 export default NothingPage;
