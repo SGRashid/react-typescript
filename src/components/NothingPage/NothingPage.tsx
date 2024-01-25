3482d37edbc56fc154eae3a56a67d8f59beb9eef
Sergei Gabdullin <sgrashid@yandex.ru>
2024-01-25 12:21:01 +0500

    Removed submodule

--- src/components/NothingPage/NothingPage.tsx
+++ src/components/NothingPage/NothingPage.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 function NothingPage(): JSX.Element {
-    return (<>
-        <h1>There is nothing here</h1>
-        <div>
-            <Link to="/">Go Home</Link>
-        </div>
-        <div>
-            <Link to="/hello">Go to HelloPage</Link>
-        </div>
-    </>);
+    return (
+        <>
+            <h1>There is nothing here</h1>
+            <div>
+                <Link to="/">Go Home</Link>
+            </div>
+            <div>
+                <Link to="/hello">Go to HelloPage</Link>
+            </div>
+        </>
+    );
 }
 
 export default NothingPage;
